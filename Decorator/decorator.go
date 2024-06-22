24c344f3702238c11f593d04faa8161177afc5f6
lr13_2002 <lr13_2002@126.com>
2024-06-22 19:58:00 +0800

    change decorator

--- Decorator/decorator.go
+++ Decorator/decorator.go
@@ -11,14 +11,14 @@ func (s Sqare) Draw() string {
 }
 
 type ColorSquare struct {
-	square IDraw
+	IDraw
 	color  string
 }
 
 func (c ColorSquare) Draw() string {
-	return c.square.Draw() + ", color is " + c.color
+	return c.IDraw.Draw() + ", color is " + c.color
 }
 
 func NewColorSquare(square IDraw, color string) ColorSquare {
-	return ColorSquare{square: square, color: color}
+	return ColorSquare{IDraw: square, color: color}
 }
